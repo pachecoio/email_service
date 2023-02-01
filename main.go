45ba86cd20517a81e10048b3cc165518e6471569
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2023-02-01 14:37:03 -0500

    Included example in the main app

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"github.com/pachecoio/email_service/adapters/mailgun"
+	"github.com/pachecoio/email_service/domain"
+	"github.com/pachecoio/email_service/services"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// Load env variables
+	err := LoadEnv()
+	if err != nil {
+		panic(err)
+	}
+	domainValue := os.Getenv("MAILGUN_DOMAIN")
+	apiKeyValue := os.Getenv("MAILGUN_API_KEY")
+
+	//	Send mail with mailgun
+	c := mailgun.NewClient(domainValue, apiKeyValue)
+	payload := &domain.EmailPayload{
+		To:      "thiagodelimapacheco@gmail.com",
+		From:    "mailgun@" + domainValue,
+		Subject: "Test running app",
+		Body:    "Test app running fine",
+	}
+	_, err = services.SendEmail(c, payload)
+
+	if err != nil {
+		panic(err)
+	}
 }
